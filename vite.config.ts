de91cf74ef740945688db428d4d622972b438d93
Alexander Volle <alexander.volle@hotmail.com>
2025-01-04 01:53:37 +0100

    Fix paths for builds

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [viteBasicSslPlugin(), react()],
+    base: "/banebok",
 });
